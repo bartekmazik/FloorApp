65dc292396b56c6c7e54644eea05b0acbbd72641
bartek <log.mazik.bartek@gmail.com>
2024-04-22 13:47:47 +0200

    added boundaries, deployed, removed contact page

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import "../styles/Panel.css";
 const Panel = ({ elemWidth, elemHeight, content }) => {
   return (
-    <div className="Panel" style={{ width: elemWidth, height: elemHeight }}>
+    <div
+      className="Panel"
+      style={{ width: `${elemWidth / 20}rem`, height: `${elemHeight / 20}rem` }}
+    >
       {content}
     </div>
   );
